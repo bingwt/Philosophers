ae2f4b1effa396fbca40747d1305b8c9d8902b4d
Bing Wen Tan <btan@c1r3s3.42singapore.sg>
2024-02-05 09:39:26 +0800

    Initial commit

---
+++ philo/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/05 09:06:30 by btan              #+#    #+#             */
+/*   Updated: 2024/02/05 09:11:32 by btan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+#endif
