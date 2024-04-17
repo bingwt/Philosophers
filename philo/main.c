25cb8b7ed2d02584ad8bf74e14c14b078776ad31
Bing Wen Tan <btan@student.42singapore.sg>
2024-04-18 00:04:18 +0800

    new file:   philo_actions.c

--- philo/main.c
+++ philo/main.c
@@ -6,43 +6,11 @@
 /*   By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 15:20:22 by btan              #+#    #+#             */
-/*   Updated: 2024/04/16 14:46:51 by btan             ###   ########.fr       */
+/*   Updated: 2024/04/17 23:58:42 by btan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-int	p_action(t_philo *philo, t_action action)
-{
-	int	id;
-	int	timestamp;
-
-	id = philo->id;
-	timestamp = 0;
-	printf("Action: %d\n", action);
-	printf("%d ", timestamp);
-	if (action == THINK)
-		printf("%d is thinking\n", philo->no);
-	if (action == TAKE)
-	{
-		pthread_mutex_lock(&philo->rules->mutex[id]);
-		philo->rules->forks[id] = 1;
-		pthread_mutex_lock(&philo->rules->mutex[id + 1]);
-		philo->rules->forks[id + 1] = 1;
-		printf("%d has taken a fork\n", philo->no);
-	}
-	if (action == EAT)
-	{
-		printf("%d is eating\n", philo->no);
-		pthread_mutex_lock(&philo->rules->mutex[id]);
-		philo->rules->forks[id] = 0;
-		pthread_mutex_lock(&philo->rules->mutex[id + 1]);
-		philo->rules->forks[id + 1] = 0;
-	}
-	if (action == SLEEP)
-		printf("%d is sleeping\n", philo->no);
-	return (0);
-}
 
 void	*routine(void *philo)
 {
